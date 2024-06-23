c154ad404452fb31bbca701cffafeb8aed601d6f
Jhonny Timaure <164609178+TIMAURE@users.noreply.github.com>
2024-06-23 09:16:52 +0000

    segundo commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 
 
 //create your first component
 const Home = () => {
+	const[todo,settodo] = useState([]);
 const[inputValue, setInputValue] = useState ("");
-const[todo,settodo] = useState([]);
+
+useEffect(() => {
+	showTasks();
+}, []);
+
+async function showTasks() {
+	const response = await fetch(`https://glorious-invention-q7v6jp7pr5x63x5j9-3000.app.github.dev/users/TIMAURE`);
+	const data = await response.json();
+	const newTodos = data.todos;
+	settodo(newTodos);
+}
+
+const addTask = async () => {
+	if (!inputValue) {
+		alert("Por Favor Agrega Una Tarea");
+		return;
+	}
+
+	const body = {
+		label: inputValue,
+		"is_done": false
+	};
+
+	try {
+		const response = await fetch("https://glorious-invention-q7v6jp7pr5x63x5j9-3000.app.github.dev/todos/TIMAURE", {
+			method: "POST",
+			headers: {
+				"accept": "application/json",
+				"Content-Type": "application/json"},
+			body: JSON.stringify(body)});
+		
+			const data = await response.json();
+		settodo([...todo, data]);
+	} catch (error) {
+		console.log(error);
+	}
+	setInputValue("");
+};
+
+
 
 	return (
 		<div className="contenedo">
@@ -17,7 +57,7 @@ const[todo,settodo] = useState([]);
 	onKeyDown={(e) =>{if (e.key === "Enter"){settodo(todo.concat([inputValue]));
 	setInputValue("");}} } ></input>
 	</li>
-	{todo.length === 0 ? (<li>"No hay tareas, añadir tareas"</li>) : todo.map((item, index) =><li className="conjunto">{item}{""}
+	{todo.length === 0 ? (<li>"No hay tareas, añadir tareas"</li>) : todo.map((item, index) =><li className="conjunto" >{item}{""}
 		<i className="fa-solid fa-x" 
 		onClick={() => 
 		settodo(todo.filter((t, currentindex)=> index != currentindex))}>
@@ -27,3 +67,7 @@ const[todo,settodo] = useState([]);
 </div>);};
 
 export default Home;
+
+
+
+
